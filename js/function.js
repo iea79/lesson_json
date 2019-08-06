5a7041cbfcbf11e192e83c406f833f0b3cbb7a20
Evgeniy Ivanov <busforward@gmail.com>
2019-08-06 21:14:51 +0500

    Day 2

--- js/function.js
+++ js/function.js
@@ -1,84 +1,62 @@
-let list = $('.productList'),
-    hello = 'Hello',
-    price = 2000 * 1.3,
-    listItem = $('.productList__item');
+let url = 'https://script.google.com/macros/s/AKfycbzMyVjjQzwfMXfJLcSlWbUsoIk43tZlXzmuE210gOnYaew5pDg/exec',
+    appData = [],
+    prodList = $('.productList'),
+    template = '';
 
-// console.log(price);
+// console.log($.get(url));
+
+$.getJSON(url, function(json, status) {
+    appData = json.result;
+
+    prodList.html('');
+
+    // console.log(status);
+    // console.log(json);
+    // console.log(json.result);
 
 
-let arr = {
-    name: 'Jonh',
-    age: 25,
-    skill: ['html', 'js', 'css']
-};
+    for (let i = 0; i < appData.length; i++) {
 
-// console.log(arr);
+        template = `<div class="productList__item">
+              <div class="productList__content">
+                <div class="productList__img"><img src="`+appData[i][4]+`" alt=""></div>
+                <div class="productList__description">
+                  <div class="productList__name">`+appData[i][1]+`</div>
+                  <div class="productList__price">$`+appData[i][2]+`</div>
+                  <div class="productList__action">
+                    <button class="btn" type="button">bUY NOW</button>
+                  </div>
+                </div>
+              </div>
+            </div>`;
 
-let num = '4';
+        if (i > 5) {
+            return false;
+        } else {
+            prodList.append(template);
+        }
 
-if (num < 5) {
-    // console.log('< 5');
-}
-if (num == 5) {
-    // console.log('= 5');
-}
-if (num > 5) {
-    // console.log('> 5');
-}
-
-switch (num) {
-    case '5':
-        // console.log('5');
-        break;
-    case '6':
-        // console.log('6');
-        break;
-    case '7':
-        // console.log('7');
-        break;
-    default:
-        // console.log('Нет совпадений');
-}
-
-function getList(item) {
-
-    console.log(item.length + ' товаров');
-}
-
-$('body').on('click', '.js_showMore', function(event) {
-    getList($('.header__nav li'));
-    listItem.toggleClass('active');
-});
-
-$('.productList__action .btn').on('click', function() {
-    let listItem = $(this).closest('.productList__item');
-
-    // listItem.toggleClass('active');
-
-    // listItem.find('.productList__name').remove();
-
-    console.log(listItem.index());
+    }
 
 });
 
-let btn = `<div class="productMore">
-            <button class="btn btn_success js_showMore" type="submit">show More item</button>
-        </div>`;
 
-// list.append(btn);
-
-// list.html(btn);
-
-
-let numArr = [1,2,3,4,5];
+$('.js_showMore').on('click', function() {
+    $.getJSON(url, function(json, status) {
+        appData = json.result;
+        // console.log(status);
+        // console.log(json);
+        // console.log(json.result);
 
 
-let itemTempl = `<div class="productList__item">
+        for (let i = 6; i < appData.length; i++) {
+
+            template = `<div class="productList__item">
                   <div class="productList__content">
-                    <div class="productList__img"><img src="img/bug1.png" alt=""></div>
+                    <div class="productList__img"><img src="`+appData[i][4]+`" alt=""></div>
                     <div class="productList__description">
-                      <div class="productList__name">EMS Women bAG</div>
-                      <div class="productList__price">$300</div>
+                      <div class="productList__name">`+appData[i][1]+`</div>
+                      <div class="productList__price">$`+appData[i][2]+`</div>
                       <div class="productList__action">
                         <button class="btn" type="button">bUY NOW</button>
                       </div>
@@ -86,6 +64,8 @@ let itemTempl = `<div class="productList__item">
                   </div>
                 </div>`;
 
-for (var i = 0; i < numArr.length; i++) {
-    list.append(itemTempl);
-}
+            prodList.append(template);
+
+        }
+    });
+})
